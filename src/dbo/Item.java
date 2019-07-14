6fc7a46c562e9391bc70bcfb349180f7c8ebdcbd
Hung Tuan <hungtuan.user@gmail.com>
2019-07-14 13:32:27 +0700

    adding class (2)

--- src/dbo/Item.java
+++ src/dbo/Item.java
@@ -8,4 +8,65 @@ public class Item {
     private String unit = ""; 
     private int price = 0;
     private boolean supplying = false;
+
+    public Item() {
+    }
+    public Item(String itemCode, String itemName, Supplier supplier, String unit, int price, boolean supplying) {
+        this.itemCode = itemCode;
+        this.itemName = itemName;
+        this.supplier = supplier;
+        this.unit = unit;
+        this.price = price;
+        this.supplying = supplying;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isSupplying() {
+        return supplying;
+    }
+
+    public void setSupplying(boolean supplying) {
+        this.supplying = supplying;
+    }
+    
+    
 }
--- src/dbo/Items.java
+++ src/dbo/Items.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Item List
 package dbo;
+import java.util.*;
+import java.sql.*;
 
-/**
- *
- * @author tuannnh
- */
-public class Items {
+public class Items extends Vector<Item>{
+    final int SUPPLYING = 1;
+    final int NOTSUPPLYING = 2;
+
+    public Items() {
+        super();
+    }
+    
+    public int find (String itemCode){
+        for (int i = 0; i < this.size(); i++) {
+            if(itemCode.equals(this.get(i).getItemCode()))
+                return i;
+        }
+        return -1;
+    }
+    
+    public Item findItem(String itemCode){
+        int i = find(itemCode);
+        return i<0? null : this.get(i);
+    }
+    
+    public void loadFromDB(ItemDBAccess dbObj, Suppliers suppliers, int supply){
+        
+    }
+    
     
 }
